95476501f0b6408d83d8f71fcee59cbb3108c450
wellinton1 <skyred1@gmail.com>
2019-12-21 18:57:36 -0300

    Update aex.mk

--- aex.mk
+++ aex.mk
@@ -1,11 +1,4 @@
-
-TARGET_RIL_VARIANT := caf
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/aosp/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-
-# GoogleCamera
-PRODUCT_PACKAGES += \
-	GoogleCameraMod
