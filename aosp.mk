4a7d3bd7d4c15163bcadd724bb4611e73009d93f
wellinton1 <skyred1@gmail.com>
2020-01-05 18:59:26 -0300

    aosip.mk

---
+++ aosp.mk
@@ -0,0 +1,9 @@
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+$(call inherit-product, device/aosip/sepolicy/common/sepolicy.mk)
+-include vendor/aosip/build/core/config.mk
+
+PRODUCT_PACKAGES += telephony-ext
+PRODUCT_BOOT_JARS += telephony-ext
+AOSIP_BUILDTYPE := GSI
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 
