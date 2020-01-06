3e43daf5c6a288c7ed001de603edc59b817dbc4b
wellinton1 <skyred1@gmail.com>
2020-01-06 08:20:03 -0300

    Create bootleggers.mk

---
+++ bootleggers.mk
@@ -0,0 +1,6 @@
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
+-include vendor/bootleggers/build/core/config.mk
+-include vendor/bootleggers/build/core/apicheck.mk
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1280
